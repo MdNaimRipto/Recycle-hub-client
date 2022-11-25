44991a3c4df4e1597401d140d5efe278c2341c02
MdNaimRipto <naimurtsc567@gmail.com>
2022-11-25 16:59:14 +0600

    Book Now Form created

--- src/Pages/Shared/BuyNowModal.js
+++ src/Pages/Shared/BuyNowModal.js
@@ -1,6 +1,8 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BuyNowModal = () => {
+
+const BuyNowModal = ({ details }) => {
     return (
         <>
             {/* The button to open modal */}
@@ -10,8 +12,27 @@ const BuyNowModal = () => {
             <div className="modal">
                 <div className="modal-box bg-accent relative">
                     <label htmlFor="buyNow-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <h3 className="text-lg font-bold my-3">Please Complete the Form</h3>
+                    <form >
+                        <input name="name" type="text" placeholder="Full Name"
+                            className="input w-full mx-auto mb-4" disabled />
+                        <input name="email" type="email" placeholder="Email Address"
+                            className="input w-full mx-auto mb-4" disabled />
+                        {
+                            // user?.uid &&
+                            <input name="itemName"
+                                className="input w-full mx-auto mb-4" disabled />
+                        }
+                        <input name="phone" type="tel" placeholder="Phone Number"
+                            className="input w-full mx-auto mb-4" />
+                        <input name="phone" type="tel" placeholder="Meeting Location"
+                            className="input w-full mx-auto mb-4" />
+                        <button
+                            type='submit'
+                            className='btn btn-primary text-white font-semibold w-full'>
+                            Add to Cart
+                        </button>
+                    </form>
                 </div>
             </div>
         </>
