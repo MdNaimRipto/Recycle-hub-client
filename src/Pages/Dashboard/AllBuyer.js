963c50c3e18c193c97861efe8881027c6a2d8da7
MdNaimRipto <naimurtsc567@gmail.com>
2022-11-28 12:57:43 +0600

    Buyer Delete function added

--- src/Pages/Dashboard/AllBuyer.js
+++ src/Pages/Dashboard/AllBuyer.js
@@ -1,10 +1,71 @@
 import React from 'react';
+import swal from "sweetalert";
+import toast from 'react-hot-toast';
 
-const AllBuyer = () => {
+const AllBuyer = ({ allBuyer, index, refetch }) => {
+
+    const handleDelete = (_id) => {
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this Buyer!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    fetch(`http://localhost:5000/users/${allBuyer._id}`, {
+                        method: "DELETE",
+                        headers: {
+                            authorization: `bearer ${localStorage.getItem("token")}`
+                        }
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            if (data.deletedCount === 1) {
+                                console.log(data)
+                                swal(`Selected Buyer has been deleted!`, {
+                                    icon: "success",
+                                });
+                                refetch()
+                            }
+                            else {
+                                toast.error("Could not Delete. Please Try Again")
+                            }
+                        })
+                }
+            });
+    }
+
     return (
-        <div>
-
-        </div>
+        <>
+            {
+                allBuyer.user.role === "buyer" &&
+                <tr key={allBuyer._id}>
+                    <td>{index + 1}</td>
+                    <td>
+                        <div className="flex items-center space-x-3">
+                            <div className="avatar">
+                                <div className="mask mask-squircle w-12 h-12">
+                                    <img src={allBuyer.user.img} alt="Avatar Tailwind CSS Component"
+                                        referrerPolicy='no-referrer'
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>{allBuyer.user.name}</td>
+                    <td>{allBuyer.user.email}</td>
+                    <td>
+                        <button
+                            onClick={() => { handleDelete(allBuyer._id) }}
+                            className="btn btn-circle btn-sm btn-primary btn-outline">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                        </button>
+                    </td>
+                </tr>
+            }
+        </>
     );
 };
 
--- src/Pages/Dashboard/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import Loading from '../Shared/Loading';
+import AllBuyer from './AllBuyer';
 
 const AllBuyers = () => {
-    const { data: allBuyers, isLoading } = useQuery({
+    const { data: allBuyers, isLoading, refetch } = useQuery({
         queryKey: ["users"],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/users', {
@@ -44,30 +45,12 @@ const AllBuyers = () => {
                                     <tbody>
                                         {
                                             allBuyers.map((allBuyer, index) =>
-                                                allBuyer.user.role === "buyer" &&
-                                                <tr key={allBuyer._id}>
-                                                    <td>{index + 1}</td>
-                                                    <td>
-                                                        <div className="flex items-center space-x-3">
-                                                            <div className="avatar">
-                                                                <div className="mask mask-squircle w-12 h-12">
-                                                                    <img src={allBuyer.user.img} alt="Avatar Tailwind CSS Component"
-                                                                        referrerPolicy='no-referrer'
-                                                                    />
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                    <td>{allBuyer.user.name}</td>
-                                                    <td>{allBuyer.user.email}</td>
-                                                    <td>
-                                                        <button
-                                                            // onClick={() => { handleDelete(myProduct._id) }}
-                                                            className="btn btn-circle btn-sm btn-primary btn-outline">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                                                        </button>
-                                                    </td>
-                                                </tr>
+                                                <AllBuyer
+                                                    allBuyer={allBuyer}
+                                                    index={index}
+                                                    key={allBuyer._id}
+                                                    refetch={refetch}
+                                                />
                                             )
                                         }
                                     </tbody>
